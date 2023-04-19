c1e275199a4e5c82d0a24609e2f9028b77d44313
Nikhil Dev A S <asdevnikhil@gmail.com>
2023-04-19 18:27:09 +0530

    change api url again

--- pages/utils/url.js
+++ pages/utils/url.js
@@ -1,2 +1,2 @@
-export const Apiurl = 'https://nextjs-ecommerce-application-azure.vercel.app';
-// export const Apiurl = 'http://localhost:3000'
+// export const Apiurl = 'https://nextjs-ecommerce-application-azure.vercel.app';
+export const Apiurl = 'http://127.0.0.1:3000';
